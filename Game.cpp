d1f11abbf36c044073b2aa4e2dd6ec46d1311827
chrar216 <chrar216@student.liu.se>
2015-03-02 17:18:15 +0100

    version 0.2 fully functional, simple features

--- Game.cpp
+++ Game.cpp
@@ -16,12 +16,6 @@ Game::Game(sf::RenderWindow* window_) {
     infoText.setColor(sf::Color::Magenta);
     infoText.setPosition(100, 100);
     
-//    wordText.setFont(font);
-//    wordText.setCharacterSize(50);
-//    wordText.setStyle(sf::Text::Bold);
-//    wordText.setColor(sf::Color::Red);
-//    wordText.setPosition(300, 300);
-    
     scoreText.setFont(font);
     scoreText.setCharacterSize(30);
     scoreText.setStyle(sf::Text::Bold);
@@ -59,20 +53,9 @@ string toString(type number, int precision) {
     return out.str();
 }
 
-void Game::nextWord() {
-    currentWord = dictionary->getRandom();
-	
-    cout << "NEXT WORD!" << endl;
-//    richText.clear();
-//    richText << sf::Text::Bold << sf::Color::Red << currentWord;
-//    window->draw(richText);
-    
-//    typingWord = "";
-}
-
 void Game::keyPressed(sf::Event event) {
     if(event.type == sf::Event::KeyPressed) {
-        if(event.key.code >= 0 && event.key.code <= 25) {
+        if(event.key.code >= 0 && event.key.code <= 25 && !gameOver) {
             typingWord += dictionary->toASCII(event.key.code);
             
             // Check how many correct letters has been typed
@@ -95,7 +78,6 @@ void Game::keyPressed(sf::Event event) {
                     typingWord = "";
                     currentWord = dictionary->getRandom();
                     
-        //                score -= scoreInc;
                     wordTime -= 5;
                     scoreText.setString(toString(score, 2) + " points");
                     break;
@@ -105,8 +87,6 @@ void Game::keyPressed(sf::Event event) {
             richText.clear();
             richText << sf::Text::Bold << sf::Color::Green << currentWord.substr(0, length)
                      << sf::Text::Bold << sf::Color::Red << currentWord.substr(length, currentWord.length());
-        
-        
     	}
         else if(event.key.code == sf::Keyboard::Return && gameOver) {
             init();
@@ -118,7 +98,6 @@ void Game::run() {
     elapsed = clock.getElapsedTime();
     window->draw(infoText);
     window->draw(scoreText);
-    window->draw(richText);
     
     if(!started) {
 		if(elapsed.asSeconds() < prepTime) {
@@ -130,6 +109,7 @@ void Game::run() {
         }
     }
     else if(!gameOver) {
+        window->draw(richText);
         infoText.setString(toString(wordTime - elapsed.asSeconds(), 1));
         if(wordTime - elapsed.asSeconds() <= 0) {
             infoText.setString("You scored " + toString(score, 2) + " points\nPress enter to restart");
